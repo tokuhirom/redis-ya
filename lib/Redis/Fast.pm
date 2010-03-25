5643251be00c0a2f6b5647928d081666b6a786bb
tokuhirom <tokuhirom@gmail.com>
2010-03-26 01:56:03 +0900

    initial import

---
+++ lib/Redis/Fast.pm
@@ -0,0 +1,39 @@
+package Redis::Fast;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Redis::Fast -
+
+=head1 SYNOPSIS
+
+  use Redis::Fast;
+
+=head1 DESCRIPTION
+
+Redis::Fast is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
